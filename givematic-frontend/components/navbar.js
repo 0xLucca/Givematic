aec6eab99cdd984cf7baa7078d771deb92cff301
Rask467 <conner.jensen@hey.com>
2022-06-03 22:10:54 -0600

    Add charity to navbar

--- givematic-frontend/components/navbar.js
+++ givematic-frontend/components/navbar.js
@@ -3,7 +3,10 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 function Navbar() {
   return (
     <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-800 flex justify-between">
-      <span className="text-3xl text-white">GIVEMATIC</span>
+      <div className="flex items-center">
+        <span className="text-3xl text-white"><a href="/">GIVEMATIC</a></span>
+        <span className="text-lg ml-4 text-white"><a href="/charity">Charity</a></span>
+      </div>
       <ConnectButton />
     </nav>
   )
